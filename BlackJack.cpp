c2723f5dd0505440be8460927ec40296761a34e6
rednikeeg <maks.schoolforvv@gmail.com>
2021-03-14 13:52:33 +0200

    Refactored and optimized

--- BlackJack.cpp
+++ BlackJack.cpp
@@ -20,9 +20,7 @@
   #define DIAMOND "\xE2\x99\xA6"
 #endif
 
-using namespace std;
-
-std::vector<string> deck;
+std::vector<std::string> deck;
 std::map<char, int> valueMap = {
   {'2', 2},
   {'3', 3},
@@ -40,7 +38,7 @@ std::map<char, int> valueMap = {
 };
 
 void formDeck(){
-  std::vector<string> ranks = {
+  std::vector<std::string> ranks = {
     "2",
     "3",
     "4",
@@ -56,27 +54,27 @@ void formDeck(){
     "A"
   };
 
-  std::vector<string> suits = {
+  std::vector<std::string> suits = {
     SPADE,
     CLUB,
     HEART,
     DIAMOND
   };
 
-  for(int i = 0; i < 4; i++)
-    for(int j = 0; j < 13; j++)
-      deck.push_back(ranks[j] + suits[i]);
+  for(std::string suit : suits)
+    for(std::string rank : ranks)
+      deck.push_back(rank + suit);
 }
 
 class Hand{
 private:
-  std::vector<string> cards;
+  std::vector<std::string> cards;
   bool isDiler = 0;
   unsigned short softness = 0, aces = 0;
 public:
   unsigned short value = 0;
   bool defeat = 0;
-  bool blackJack = 0;
+  bool blackjack = 0;
 
   void dealer(){
     isDiler = 1;
@@ -98,37 +96,37 @@ public:
     check();
   }
 
-  string show(){
-    string hand;
+  std::string show(){
+    std::string hand = "\n";
     int displayValue;
 
     if(isDiler){
-      hand = "Dealer has " + cards[0] + (cards.size() > 1 ? (" and " + to_string(cards.size() - 1) + " other cards") : "");
+      hand += "Dealer has " + cards[0] + (cards.size() > 1 ? (" and " + std::to_string(cards.size() - 1) + " other cards") : "");
       displayValue = valueMap[cards[0][0]];
     }
     else{
       displayValue = value;
-      hand = "Your hand: ";
-      for(int i = 0; i < cards.size(); i++)
-        hand += cards[i] + ' ';
+      hand += "Your hand: ";
+      for(std::string card : cards)
+        hand += card + ' ';
     }
-    hand += "\nValue of hand - " + to_string(displayValue) + "\n\n";
+    hand += "\nValue of hand - " + std::to_string(displayValue) + "\n\n";
     return hand;
   }
 
-  string lastShow(){
-    string hand = "";
+  std::string lastShow(){
+    std::string hand = "\n";
 
     if(isDiler)
       hand += "Dealer's hand:\n";
     else
       hand += "Your hand:\n";
 
-    for(int i = 0; i < cards.size(); i++){
-      hand += cards[i] + ' ';
+    for(std::string card : cards){
+      hand += card + ' ';
     }
 
-    hand += "\nValue of hand - " + to_string(value) + '\n';
+    hand += "\nValue of hand - " + std::to_string(value) + '\n';
 
     return hand;
   }
@@ -149,7 +147,7 @@ public:
         defeat = 1;
     }
     else if(value == 21 && cards.size() == 2)
-      blackJack = 1;
+      blackjack = 1;
   }
 };
 
@@ -157,46 +155,60 @@ int main(){
 
   formDeck();
 
-  string answer;
+  std::string answer;
 
   Hand player, dealer;
 
   dealer.dealer();
 
-  cout << "Welcome to Black Jack!\n";
+  std::cout << "Welcome to Black Jack!\n";
 
   dealer.start();
   player.start();
 
-  cout << dealer.show() << player.show() << '\n';
+  std::cout << dealer.show() << player.show() << '\n';
 
+  //Making final hand for dealer
   while(dealer.value < 16)
     dealer.newCard();
 
-  while(!player.defeat || player.value != 21){
-
+  while(!player.defeat && player.value != 21){
+    //Player's move
     do {
-      cout << "Hit or stand?\n";
-      cin >> answer;
+      std::cout << "Hit or stand?\n";
+      std::cin >> answer;
     }
-    while(answer[0] != 'h' && answer[0] != 's' && !player.defeat);
-
+    while(answer[0] != 'h' && answer[0] != 's');
 
     if(answer[0] == 'h')
       player.newCard();
     else
       break;
-    cout << '\n' << dealer.show() << player.show() << '\n';
+    //Game at the moment
+    std::cout << dealer.show() << player.show();
 
   }
 
-  cout << '\n' << dealer.lastShow() << player.lastShow() << '\n';
+  std::cout << dealer.lastShow() << player.lastShow() << '\n';
 
+  //Defeat
   if(player.defeat || (player.value < dealer.value && !dealer.defeat))
-    cout << "You've last.\n";
-  else {
-    cout << "You won!\n";
+    std::cout << "You've lost.\n";
+  //Endgame by blackjack
+  else if(player.value == dealer.value) {
+    if(!player.blackjack && dealer.blackjack){
+      std::cout << "Values of hands are the same, but dealer got the blackjack.\nYou've last.\n";
+    }
+    else if (player.blackjack && !dealer.blackjack){
+      std::cout << "Values of hands are the same, but you got the blackjack.\nYou've won.\n";
+    }
+    else{
+      std::cout << "It's draw.\n";
+    }
   }
-
+  //Victory
+  else{
+    std::cout << "You've won.\n";
+  }
   return 0;
 }
